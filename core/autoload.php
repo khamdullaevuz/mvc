06ba45e33b1770c3a868141f984e4443ca1c720b
khamdullaevuz <elbek.khamdullaev@gmail.com>
2022-08-11 14:47:07 +0500

    fixed autoloading

--- core/autoload.php
+++ core/autoload.php
@@ -1,8 +1,8 @@
 <?php
-
+g
 spl_autoload_register(function($class){
-    require '../'.str_replace('\\', '/', $class).'.php';
+    require '../'.$class.'.php';
 });
 
-require '../routes/web.php';
 require '../config/core.php';
+require '../routes/web.php';
