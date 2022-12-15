6f5ee8a8a89fb2cec6139b1e591c89b0d56acc60
Elbek Khamdullaev <elbek.khamdullaev@gmail.com>
2022-12-15 13:19:58 +0500

    moving config outside core

--- config/Core.php
+++ config/Core.php
@@ -1,6 +1,8 @@
 <?php
 
-class Config
+namespace Config;
+
+class Core
 {
     const APP_NAME = "MVC";
 
--- core/Connection.php
+++ core/Connection.php
@@ -1,12 +1,14 @@
 <?php
 
+use Config\Core as Config;
+
 abstract class Connection
 {
     protected static PDO $pdo;
 
     function __construct()
     {
-        if(empty(self::$pdo)){
+        if (empty(self::$pdo)) {
             self::connect();
         }
     }
