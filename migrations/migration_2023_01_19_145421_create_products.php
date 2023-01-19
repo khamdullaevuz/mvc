4766fc5eaa733ff9ecbb8dbb5851da8879f2c7a5
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-01-19 21:35:35 +0500

    customized some codes

--- migrations/migration_2023_01_19_145421_create_products.php
+++ migrations/migration_2023_01_19_145421_create_products.php
@@ -3,14 +3,14 @@
 class migration_2023_01_19_145421_create_products extends Migration
 {
     public function up()
-        {
-            $this->create('products', [
-                'id' => 'INT AUTO_INCREMENT PRIMARY KEY',
-                'name' => 'VARCHAR(255) NOT NULL',
-                'created_at' => 'DATETIME',
-                'updated_at' => 'DATETIME',
-            ]);
-        }
+    {
+        $this->create('products', [
+            'id' => 'INT AUTO_INCREMENT PRIMARY KEY',
+            'name' => 'VARCHAR(255) NOT NULL',
+            'created_at' => 'DATETIME',
+            'updated_at' => 'DATETIME',
+        ]);
+    }
 
     public function down()
     {
